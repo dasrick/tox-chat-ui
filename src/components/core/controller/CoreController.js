e29fa8fa594791df07f063c9c349db5f68bf9032
Enrico Hoffmann <dasrick@gmail.com>
2015-12-16 21:56:29 +0100

    minor updates for ui

--- src/components/core/controller/CoreController.js
+++ src/components/core/controller/CoreController.js
@@ -10,79 +10,98 @@ module.exports = function () {
   vm.messages = [
     {
       'username': 'Matt',
-      'content': 'Hi!'
+      'content': 'Hi!',
+      'date': 1450254829
     },
     {
       'username': 'Elisa',
-      'content': 'Whats up?'
+      'content': 'Whats up?',
+      'date': 1450264829
     },
     {
       'username': 'Matt',
-      'content': 'I found this nice AngularJS Directive'
+      'content': 'I found this nice AngularJS Directive',
+      'date': 1450274829
     },
     {
       'username': 'Matt',
-      'content': 'Hi!'
+      'content': 'Hi!',
+      'date': 1450284829
     },
     {
       'username': 'Elisa',
-      'content': 'Whats up?'
+      'content': 'Whats up?',
+      'date': 1450290829
     },
     {
       'username': 'Matt',
-      'content': 'I found this nice AngularJS Directive'
+      'content': 'I found this nice AngularJS Directive',
+      'date': 1450291829
     },
     {
       'username': 'Matt',
-      'content': 'Hi!'
+      'content': 'Hi!',
+      'date': 1450292829
     },
     {
       'username': 'Elisa',
-      'content': 'Whats up?'
+      'content': 'Whats up?',
+      'date': 1450293829
     },
     {
       'username': 'Matt',
-      'content': 'I found this nice AngularJS Directive'
+      'content': 'I found this nice AngularJS Directive',
+      'date': 1450293829
     },
     {
       'username': 'Matt',
-      'content': 'Hi!'
+      'content': 'Hi!',
+      'date': 1450293929
     },
     {
       'username': 'Elisa',
-      'content': 'Whats up?'
+      'content': 'Whats up?',
+      'date': 1450294029
     },
     {
       'username': 'Matt',
-      'content': 'I found this nice AngularJS Directive'
+      'content': 'I found this nice AngularJS Directive',
+      'date': 1450294129
     },
     {
       'username': 'Matt',
-      'content': 'Hi!'
+      'content': 'Hi!',
+      'date': 1450294229
     },
     {
       'username': 'Elisa',
-      'content': 'Whats up?'
+      'content': 'Whats up?',
+      'date': 1450294329
     },
     {
       'username': 'Matt',
-      'content': 'I found this nice AngularJS Directive'
+      'content': 'I found this nice AngularJS Directive',
+      'date': 1450294429
     },
     {
       'username': 'Matt',
-      'content': 'Hi!'
+      'content': 'Hi!',
+      'date': 1450294529
     },
     {
       'username': 'Elisa',
-      'content': 'Whats up?'
+      'content': 'Whats up?',
+      'date': 1450294629
     },
     {
       'username': 'Matt',
-      'content': 'I found this nice AngularJS Directive'
+      'content': 'I found this nice AngularJS Directive',
+      'date': 1450294729
     },
     {
-      'username': 'Elisa',
-      'content': 'Looks Great!'
+      'username': 'Enrico Hoffmann von Fallersleben',
+      'content': 'Looks Great!',
+      'date': 1450294829
     }
   ];
 
@@ -92,7 +111,8 @@ module.exports = function () {
     if (message && message !== '' && username) {
       vm.messages.push({
         'username': username,
-        'content': message
+        'content': message,
+        'date': Date.now()
       });
     }
   }
